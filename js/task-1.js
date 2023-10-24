8fe42b90ea8c5c0da84fb59ad8a79ac89c22e20c
Williannoterdan <136361912+Williannoterdan@users.noreply.github.com>
2023-10-24 17:22:04 +0300

    fixs

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,16 @@
-"use strict";
 function makeTransaction(quantity, pricePerDroid) {
-  // Умова на випадок дивних значень
-  if (quantity < 0 || pricePerDroid < 0) {
-    return `Sorry, an error occurred`;
-  }
+    // Умова на випадок дивних значень
+    if (quantity < 0 || pricePerDroid < 0) {
+        return `Sorry, an error occurred`
+    }
 
-  return `You ordered ${quantity} droids worth ${
-    pricePerDroid * quantity
-  } credits!`;
+    return `You ordered ${quantity} droids worth ${
+        pricePerDroid * quantity
+    } credits!`
 }
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000))
+console.log(makeTransaction(3, 1000))
+console.log(makeTransaction(10, 500))
 // console.log(makeTransaction(10, 0));
 // console.log(makeTransaction(-1, 500));
 // console.log(makeTransaction(0, 0));
--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,18 @@
-"use strict";
 function getShippingMessage(country, price, deliveryFee) {
-  if (
-    typeof price !== "number" ||
-    typeof deliveryFee !== "number" ||
-    typeof country !== "string"
-  ) {
-    return `Sorry there was an error `;
-  }
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+    if (
+        typeof price !== 'number' ||
+        typeof deliveryFee !== 'number' ||
+        typeof country !== 'string'
+    ) {
+        return `Sorry there was an error `
+    }
+    const totalPrice = price + deliveryFee
+    return `Shipping to ${country} will cost ${totalPrice} credits`
 }
 
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+console.log(getShippingMessage('Australia', 120, 50))
+console.log(getShippingMessage('Germany', 80, 20))
+console.log(getShippingMessage('Sweden', 100, 20))
 // console.log(getShippingMessage('Sop', 100, 20));
 // console.log(getShippingMessage('Spanish', 0, 0));
 // console.log(getShippingMessage('Spanish', 10, -12));
